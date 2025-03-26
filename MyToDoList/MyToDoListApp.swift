5f802889001ad6f435de2979b18ca5a0767797ec
Chairoel <projectchairul@gmail.com>
2025-03-26 13:32:37 +0700

    Initial Commit

---
+++ MyToDoList/MyToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoListApp.swift
+//  MyToDoList
+//
+//  Created by Chairul Amri on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
