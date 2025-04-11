13cfdeb62a25433410d3efbdd44258a24725cafa
Chairoel <projectchairul@gmail.com>
2025-04-11 09:54:26 +0700

    setup done check marks toggle

--- MyToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ MyToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Chairul Amri on 26/03/25.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for single to do list item view (each row in items list)
@@ -14,6 +16,18 @@ class ToDoListItemViewViewModel: ObservableObject {
     }
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictrionary())
     }
 }
